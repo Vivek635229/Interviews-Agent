4e1bbd310b343b850f8dac92916f61a37777d663
Vivek J Khara <21amtics140@gmail.com>
2026-07-11 20:49:47 +0530

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+   babel: {
+    plugins: [
+      // Only enable react-refresh in development
+      process.env.NODE_ENV === 'development' && 'react-refresh/babel',
+    ].filter(Boolean), // This removes the 'false' value in production
+  },
   style: {
     postcss: {
       mode: 'extends',
